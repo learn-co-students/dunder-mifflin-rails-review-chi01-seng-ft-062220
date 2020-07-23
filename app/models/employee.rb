a81802445d807823547f28965a4f0228909d87e2
foxten <tenaysiafox@gmail.com>
2020-07-22 22:59:29 -0500

    Done.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,17 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :title, uniqueness: true
+    validates :alias, uniqueness: true, if: :alias_check
+
+    def alias_check
+        self.alias.present?
+    end
+
+    def name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def set_image
+        self.img_url = "https://images.app.goo.gl/C8UjWkTRTczWKzd4A"
+    end
 end
