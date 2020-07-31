2b18c472a9cbd83148df00520b69814c843b2fac
Simon51584 <simon51584@gmail.com>
2020-07-30 21:16:15 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :dogs
-  resources :employees
+  resources :employees, except: :delete
+
+  post '/dogs/sorted', to: 'dogs#sorted'
+  get '/dogs', to: 'dogs#sorted'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
